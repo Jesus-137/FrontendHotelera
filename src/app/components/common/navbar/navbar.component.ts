a4377929eb33be05bad3a8fc62d374fc5a8d6568
bryan carvajal <bryancarvajal747@gmail.com>
2025-09-18 12:49:30 -0600

    creacion del navbar y dashboard. ya tiene implementacion de usuarios, pero estancomentado para no causar errores

--- src/app/components/common/navbar/navbar.component.ts
+++ src/app/components/common/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+//import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -9,4 +10,15 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+  /*username: string | null = null;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.username = this.authService.getUsername();
+  }
+
+  logout(): void {
+    this.authService.logout();
+  }*/
 }
