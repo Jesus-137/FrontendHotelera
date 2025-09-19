2fe87cdfa7928c043f3e42c674720e531a273cca
bryan carvajal <bryancarvajal747@gmail.com>
2025-09-19 00:22:23 -0600

    rutas actualizadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,20 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HabitacionesComponent } from './components/habitaciones/habitaciones.component';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ReservasComponent } from './components/reservas/reservas.component';
 
 const routes: Routes = [
-  {path: 'habitaciones', component: HabitacionesComponent, pathMatch: 'full'},
+  {path:'', redirectTo: '/login', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent },
+  {
+    path: 'dashboard', component: DashboardComponent, /*canActivate: [AuthGuard]*/
+    children: [
+      {path: 'habitaciones', component: HabitacionesComponent, pathMatch: 'full'},
+      {path: 'reservaciones', component: ReservasComponent, pathMatch: 'full'},
+    ]
+  }
 ];
 
 @NgModule({
